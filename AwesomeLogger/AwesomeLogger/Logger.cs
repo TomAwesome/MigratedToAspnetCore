cf1628eb93005ad1a0b24e89f42fd4603af29831
Thomas Camargo <thomas-camargo@pluralsight.com>
2020-07-01 08:54:48 -0600

    Create logger example

---
+++ AwesomeLogger/AwesomeLogger/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using log4net;
+
+namespace AwesomeLogger
+{
+    public class Logger<T>
+    {
+        readonly ILog logger;
+
+        public Logger() : this(LogManager.GetLogger(typeof(T)))
+        {
+            
+        }
+
+        public Logger(ILog logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Error(string message, Exception ex)
+        {
+            logger.Error(message, ex);
+        }
+
+        public void Warn(string message, Exception ex)
+        {
+            logger.Warn(message, ex);
+        }
+
+        public void Info(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void Debug(string message)
+        {
+            logger.Debug(message);
+        }
+    }
+}
---
+++ AwesomeLogger/AwesomeLoggerSpecs/LoggerSpecs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AwesomeLogger;
+using log4net;
+using Moq;
+using NUnit.Framework;
+
+namespace AwesomeLoggerSpecs
+{
+    [TestFixture]
+    public class LoggerSpecs
+    {
+        [Test]
+        public void When_logging_an_error()
+        {
+            var mockLogger = new Mock<ILog>();
+            mockLogger.Setup(log => log.IsErrorEnabled).Returns(true);
+
+            var logger = new Logger<LoggerSpecs>(mockLogger.Object);
+
+            logger.Error("Bad things happened", new Exception());
+
+            mockLogger.Verify(log => log.Error(It.IsAny<string>(), It.IsAny<Exception>()));
+        }
+    }
+}
