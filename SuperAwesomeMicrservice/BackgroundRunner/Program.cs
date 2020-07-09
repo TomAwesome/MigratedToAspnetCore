b5d44e39aa4953523bd426aa0c6002b82db54402
Thomas Camargo <camargothomas@gmail.com>
2020-07-09 00:57:10 -0600

    Add a console app that will update a redis value every second.

--- SuperAwesomeMicrservice/BackgroundRunner/Program.cs
+++ SuperAwesomeMicrservice/BackgroundRunner/Program.cs
@@ -1,16 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Shared.DependencyInjection;
+using Shared.Redis;
+using StructureMap;
+using System;
 using System.Threading.Tasks;
 
 namespace BackgroundRunner
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            
+            IContainer container = new Container(config =>
+            {
+                config.AddRegistry<SharedRegistry>();
+            });
+            var redisWrapper = container.GetInstance<IRedisWrapper>();
+            while(true)
+            {
+                await redisWrapper.SaveAsync("helloworld2", DateTimeOffset.UtcNow.ToString()).ConfigureAwait(false);
+                await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+            }
+
         }
     }
 }
